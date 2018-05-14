aa0e31022eb56d39d669549d37feeada96793e1d
aditya.ratti <aditya.ratti@icreon.com>
2018-05-14 19:20:36 +0530

    New Repo Added

---
+++ Tree_SpiralLevelOrderTraversal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tree_SpiralLevelOrderTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree binaryTree = new Tree();
+            binaryTree.root = new Node(1);
+            binaryTree.root.left = new Node(2);
+            binaryTree.root.right = new Node(3);
+            binaryTree.root.left.left = new Node(4);
+            binaryTree.root.left.right = new Node(5);
+            binaryTree.root.right.left = new Node(7);
+            binaryTree.root.right.right = new Node(8);
+
+            binaryTree.horizontalTraversal(binaryTree.root);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tree_SpiralLevelOrderTraversal/Tree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree_SpiralLevelOrderTraversal
+{
+    class Node
+    {
+        public int data;
+        public Node left, right;
+
+        public Node(int d)
+        {
+            data = d;
+        }
+    }
+
+    class Tree
+    {
+        public Node root;
+
+        public void horizontalTraversal(Node node)
+        {
+            Stack<Node> stack = new Stack<Node>();
+            Queue<Node> queue = new Queue<Node>();
+
+            bool isLeft = true;
+
+            stack.Push(node);
+
+            while (stack.Count > 0 || queue.Count > 0)
+            {
+                Node dataNode;
+
+                dataNode = isLeft ? stack.Pop() : queue.Dequeue();
+
+                if (isLeft)
+                {
+                    if (dataNode.left != null)
+                        queue.Enqueue(dataNode.left);
+                    if (dataNode.right != null)
+                        queue.Enqueue(dataNode.right);
+                }
+                else
+                {
+                    if (dataNode.left != null)
+                        stack.Push(dataNode.left);
+                    if (dataNode.right != null)
+                        stack.Push(dataNode.right);
+                }
+
+                if (stack.Count == 0 && queue.Count > 0)
+                    isLeft = false;
+
+                if (queue.Count == 0 && stack.Count > 0)
+                    isLeft = true;
+
+                Console.Write(dataNode.data + " ");
+
+            }
+        }
+    }
+}
